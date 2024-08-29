84ff7161ba61b1f72986e5c86afaf8d034e13dcf
YJyoungjin <162400286+YJyoungjin@users.noreply.github.com>
2024-08-29 11:45:26 +0900

    7장 객체지향 1,2

---
+++ src/cat/Cat.java
@@ -0,0 +1,32 @@
+package cat;
+
+public class Cat {
+        private String name;
+        private String breeds;
+        private double weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreeds() {
+        return breeds;
+    }
+
+    public void setBreeds(String breeds) {
+        this.breeds = breeds;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
+
